eb4f9e17d6f40a36e5c65db30ac55ee98899e087
alfei13 <724096472@qq.com>
2017-08-23 18:09:00 +0800

    本程序记录每天学习所得

---
+++ AFAccumulating/AFAccumulating/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFAccumulating
+//
+//  Created by 郑飞飞 on 2017/8/23.
+//  Copyright © 2017年 FlyZKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFAccumulating/AFAccumulating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFAccumulating
+//
+//  Created by 郑飞飞 on 2017/8/23.
+//  Copyright © 2017年 FlyZKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
