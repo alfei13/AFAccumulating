70229b5eb7727385ebc7968ad7dc25a1105c60fd
alfei13 <724096472@qq.com>
2017-08-24 14:41:22 +0800

    添加Appitem

---
+++ AFAccumulating/AFAccumulating/AFAppItem.h
@@ -0,0 +1,19 @@
+//
+//  AFAppItem.h
+//  AFAccumulating
+//
+//  Created by 郑飞飞 on 2017/8/24.
+//  Copyright © 2017年 FlyZKing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFAppItem : NSObject
+/** name */
+@property (nonatomic, copy) NSString *name;
+
+/** title */
+@property (nonatomic, copy) NSString *title;
+
+
+@end
---
+++ AFAccumulating/AFAccumulating/AFAppItem.m
@@ -0,0 +1,15 @@
+//
+//  AFAppItem.m
+//  AFAccumulating
+//
+//  Created by 郑飞飞 on 2017/8/24.
+//  Copyright © 2017年 FlyZKing. All rights reserved.
+//
+
+#import "AFAppItem.h"
+
+@implementation AFAppItem
+
+
+
+@end
